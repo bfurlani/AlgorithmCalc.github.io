edaff79019a4b7a4cf93ab22b16ab278fec366f0
bfurlani <benjaminfurlani.1112@gmail.com>
2020-09-13 23:24:25 -0400

    added minor elements to QS page

--- src/components/quicksort-page.js
+++ src/components/quicksort-page.js
@@ -1,6 +1,17 @@
 import React from "react";
+import "../styles/qs.css";
 function QuickSortPage(props) {
-  return <h3>QuickSort</h3>;
+  return (
+    <div class="qsmain">
+      <h3>Quick Sort:</h3>
+      <br />
+      <p>
+        To start, please enter the array(do not include brackets and select the
+        seperation character) and the solution you are looking for:
+      </p>
+      <input class="qsArr" id="qsArray"></input>
+    </div>
+  );
 }
 
 export default QuickSortPage;
