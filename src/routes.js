8af011f0475eeb54926d5c2dc414d868c8c34473
bfurlani <benjaminfurlani.1112@gmail.com>
2020-09-15 12:34:57 -0400

    added routing for InsertionSortPage

--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import React from "react";
 import MergeSortPage from "./components/mergesort-page";
 import QuickSortPage from "./components/quicksort-page";
 import Home from "./components/home";
+import InsertionSortPage from "./components/insertionsort-page";
 
 const routes = {
   "/": () => <Home />,
   "/quicksort": () => <QuickSortPage />,
   "/mergesort": () => <MergeSortPage />,
   "/heapsort": () => <MergeSortPage />,
+  "/insertionsort": () => <InsertionSortPage />,
 };
 
 export default routes;
