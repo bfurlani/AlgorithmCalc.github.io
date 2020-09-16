d00326baf879ff5077887a188aa9d91574e246c9
bfurlani <benjaminfurlani.1112@gmail.com>
2020-09-16 14:08:33 -0400

    added more ghpages publish info

---
+++ build/precache-manifest.3ecf65896c01cf4473b4e00a30cc4657.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38384de4bd5cd2b54b57a4f258d372f6",
+    "url": "/AlgorithmCalc.github.io/index.html"
+  },
+  {
+    "revision": "d8c50941455351835df8",
+    "url": "/AlgorithmCalc.github.io/static/css/main.8f20fff3.chunk.css"
+  },
+  {
+    "revision": "c91b3872ea174fe36036",
+    "url": "/AlgorithmCalc.github.io/static/js/2.0f3131cc.chunk.js"
+  },
+  {
+    "revision": "7a29fd67914f99c562dc3f65bbf1078b",
+    "url": "/AlgorithmCalc.github.io/static/js/2.0f3131cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8c50941455351835df8",
+    "url": "/AlgorithmCalc.github.io/static/js/main.fad5cece.chunk.js"
+  },
+  {
+    "revision": "50ec62a1b147e15883e6",
+    "url": "/AlgorithmCalc.github.io/static/js/runtime-main.a3544eb3.js"
+  }
+]);
